c84cfe38d8ef608881ca1876eb87faa45e18c819
Jeyprox <56683209+Jeyprox@users.noreply.github.com>
2022-01-25 20:15:29 +0100

    Made some layout changes to index page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 import { useState } from "react";
 
-import { motion } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
 import CityItem from "../components/CityItem";
 
 import { HiArrowSmLeft } from "react-icons/hi";
@@ -26,17 +26,17 @@ export default function Home({ cities }) {
 
   return (
     <>
-      <section className="h-screen container mx-auto p-4 flex flex-col justify-between items-center">
-        <div className="z-10 flex flex-col items-center justify-center">
+      <section className="relative h-screen py-4 flex flex-col gap-4 justify-between items-center">
+        <div className="z-10 grid gap-2 text-center">
           <h1 className="text-gray-100 px-8 text-3xl font-bold uppercase border-b-gray-100 border-b-2">
             Lunari Empire
           </h1>
-          <h2 className="font-serif mt-2 uppercase text-gray-100 text-4xl">
+          <h2 className="font-serif uppercase text-gray-100 text-4xl">
             Nation of the Moon
           </h2>
         </div>
         <motion.div
-          className="grid w-1/2 items-center gap-16"
+          className="z-10 grid w-1/2 items-center gap-16"
           layout
           transition={{ duration: 1 }}
         >
@@ -71,8 +71,7 @@ export default function Home({ cities }) {
             </motion.div>
           )}
         </motion.div>
-        <div className="mb-8">
-          <div className="mb-2"></div>
+        <div className="z-10 mb-8">
           {landing && (
             <button onClick={() => setLanding(false)} className="btn text-2xl">
               Start the journey
@@ -83,43 +82,45 @@ export default function Home({ cities }) {
               className="btn text-2xl disabled:cursor-not-allowed hover:bg-gray-200/25"
               disabled={selectedCity == null}
             >
-              <Link
-                href={`/cities/${convertName(
-                  getCityById(selectedCity).cityName
-                )}`}
-              >
-                {selectedCity == null
-                  ? "Select a city"
-                  : `Go to ${getCityById(selectedCity).cityName}`}
-              </Link>
+              {selectedCity == null ? (
+                <p>Select a city</p>
+              ) : (
+                <Link
+                  href={`/cities/${convertName(
+                    getCityById(selectedCity).cityName
+                  )}`}
+                >
+                  <a>Go to {getCityById(selectedCity).cityName}</a>
+                </Link>
+              )}
             </button>
           )}
         </div>
-        <div className="overflow-hidden -z-10 top-0 left-0 absolute select-none w-screen h-full">
-          <Image
-            src="/img/landing.png"
-            alt="LandingImg-1"
-            layout="fill"
-            className="object-cover contrast-[0.8] saturate-[1.1] brightness-110 blur-xs scale-105"
-            priority
-            quality={75}
-          ></Image>
+        <Image
+          src="/img/landing.png"
+          alt="LandingImg-1"
+          layout="fill"
+          className="-z-10 object-cover contrast-[0.8] saturate-[1.1] brightness-110 blur-xs scale-105"
+          priority
+          quality={75}
+        ></Image>
+        <div className="z-10 absolute bottom-6 w-full px-8 flex">
+          <AnimatePresence>
+            {!landing && (
+              <motion.button
+                animate={{ opacity: 1, x: 0 }}
+                initial={{ opacity: 0, x: -100 }}
+                exit={{ opacity: 0, x: -100 }}
+                transition={{ duration: 0.5 }}
+                onClick={() => setLanding(true)}
+                className="border-gray-300 border-2 rounded-full p-1 hover:bg-gray-100/20 duration-200"
+                aria-label="Go Back"
+              >
+                <HiArrowSmLeft className="text-gray-300 text-2xl" />
+              </motion.button>
+            )}
+          </AnimatePresence>
         </div>
-        {!landing && (
-          <div className="absolute bottom-6 w-full px-8 flex">
-            <motion.button
-              animate={{ opacity: 1 }}
-              initial={{ opacity: 0 }}
-              exit={{ opacity: 0 }}
-              transition={{ duration: 0.4 }}
-              onClick={() => setLanding(true)}
-              className="border-gray-300 border-2 rounded-full p-1 hover:bg-gray-100/20 duration-200"
-              aria-label="Go Back"
-            >
-              <HiArrowSmLeft className="text-gray-300 text-2xl" />
-            </motion.button>
-          </div>
-        )}
       </section>
     </>
   );
